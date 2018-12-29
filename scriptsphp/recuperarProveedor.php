ff73e752cfa13bfd2ef37634e08bddf0f783de01
FerDev97 <fer.aravalo1997@gmail.com>
2018-12-28 21:34:15 -0600

    Ingresar producto.

--- scriptsphp/recuperarProveedor.php
+++ scriptsphp/recuperarProveedor.php
@@ -4,7 +4,7 @@ include "../config/conexion.php";
 $result = $conexion->query("SELECT * from tproveedor where id_proveedor=".$id);
 if ($result) {
     while ($fila = $result->fetch_object()) {
-            echo $fila->nombre." ".$fila->apellido;
+            echo $fila->nombre;
         }
     }else{
         echo "Error";
