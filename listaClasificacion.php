46fc3bacbaecc7ad58e5085d7096d13d2ab3409b
KevinJovel <kevinjovel9@gmail.com>
2019-01-11 11:42:19 -0600

    nueva db

--- listaClasificacion.php
+++ listaClasificacion.php
@@ -1,15 +1,16 @@
+
 <!doctype html>
 <html class="no-js" lang="">
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Ingreso de Clasificación | SISFIN</title>
+    <title>Lista de Clasificación | SISFIN</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- bootstrap select CSS
+     <!-- notification CSS
 		============================================ -->
-        <link rel="stylesheet" href="css/bootstrap-select/bootstrap-select.css">
+        <link rel="stylesheet" href="css/notification/notification.css">
     <!-- favicon
 		============================================ -->
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
@@ -47,7 +48,10 @@
     <link rel="stylesheet" href="css/wave/waves.min.css">
     <link rel="stylesheet" href="css/wave/button.css">
     <!-- main CSS
+    ============================================ -->
+     <!-- Data Table JS
 		============================================ -->
+    <link rel="stylesheet" href="css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="css/main.css">
     <!-- style CSS
 		============================================ -->
@@ -58,24 +62,65 @@
     <!-- modernizr JS
 		============================================ -->
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+
 </head>
-<script  language=JavaScript> 
+<SCRIPT  language=JavaScript> 
 function go(){
     //validacion respectiva me da hueva
-    document.form.submit(); 
-}
+    document.location.href="ingresarClasificacion.php"; 
+} 
 
-function modificar(id){
+</script> 
+<SCRIPT  language=JavaScript> 
+function notify(titulo,texto,from, align, icon, type, animIn, animOut){
+		$.growl({
+			icon: icon,
+			title: titulo+" ",
+			message: texto,
+			url: ''
+		},{
+				element: 'body',
+				type: type,
+				allow_dismiss: true,
+				placement: {
+						from: from,
+						align: align
+				},
+				offset: {
+					x: 20,
+					y: 85
+				},
+				spacing: 10,
+				z_index: 1031,
+				delay: 2500,
+				timer: 1000,
+				url_target: '_blank',
+				mouse_over: false,
+				animate: {
+						enter: animIn,
+						exit: animOut
+				},
+				icon_type: 'class',
+				template: '<div data-growl="container" class="alert" role="alert">' +
+								'<button type="button" class="close" data-growl="dismiss">' +
+									'<span aria-hidden="true">&times;</span>' +
+									'<span class="sr-only">Close</span>' +
+								'</button>' +
+								'<span data-growl="icon"></span>' +
+								'<span data-growl="title"></span>' +
+								'<span data-growl="message"></span>' +
+								'<a href="#" data-growl="url"></a>' +
+							'</div>'
+		});
+	}
+  function modificar(id){
        
        document.location.href="editarClasificacion.php?id="+id;
    }
-   
-function back(){
-    document.location.href="listaclasificacion.php"; 
-}
+	
+
 
 </script> 
-
 <body>
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -105,94 +150,75 @@ function back(){
 										<i class="notika-icon notika-form"></i>
 									</div>
 									<div class="breadcomb-ctn">
-										<h2>Registro de Clasificación.</h2>
-										<p>Formulario de clasificación <span class="bread-ntd">.</span></p>
+										<h2>Lista de Clasificacón de Activos.</h2>
+										<p>Datos <span class="bread-ntd">de Clasificación de Activos</span></p>
 									</div>
 								</div>
 							</div>
-							<!-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
 								<div class="breadcomb-report">
 									<button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="notika-icon notika-sent"></i></button>
-								</div>
-							</div> -->
+                       
+                                    <button class="btn btn-primary notika-btn-primary" onclick="go();">+ Agregar Nuevo</button>
+                                </div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<!-- Breadcomb area End-->
-    <!-- Form Examples area start-->
-    <div class="form-example-area">
+  <!-- Breadcomb area End-->
+  <!-- Data Table area Start-->
+  <div class="data-table-area">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="form-example-wrap">
-                        <div class="cmp-tb-hd">
-                            <h2>Datos de Clasificación</h2>
-                            
-                        </div>
-                        <form id="form"name="form" method="post" action="">
-                        <input type="hidden" name="bandera" id="bandera">
-                        <input type="hidden" name="baccion" id="baccion">
-                        
+                    <div class="data-table-list">
+                        <div class="basic-tb-hd">
+                             </div>
+                        <div class="table-responsive">
+                            <table id="data-table-basic" class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        
+                                        <th>Clasificaión</th>
+                                        <th>Tipo de Depreciación</th>
+                                        <th>Correlativo</th> 
+                                        <th>Modificar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                      <?php
+include "config/conexion.php";
+$result = $conexion->query("SELECT * from tclasificacion ORDER BY id_clasificaion");
+if ($result) {
+    while ($fila = $result->fetch_object()) {
+        echo "<tr>";
+     
+        echo "<td>" . $fila->nombre . "</td>";
+        echo "<td>" . $fila->tiempo_depreciacion . "</td>";
+        echo "<td>" . $fila->correlativo . "</td>";
+        echo "<td>
+        <div class='button-icon-btn'>
+        <button class='btn btn-lightgreen lightgreen-icon-notika btn-reco-mg btn-button-mg' data-dismiss='modal' onclick=\"modificar('$fila->id_clasificaion')\";><i class='notika-icon notika-menus'></i></button>
+         </div>
+        </td>";
+        echo "</tr>";
 
-                        <div class="row">
-                            <div class="col-lg-12 col-md-12 col-sm-6 col-xs-12">
-                                <div class="form-example-int">
-                                    <div class="form-group">
-                                        <label>Clasificación:</label>
-                                        <div class="nk-int-st">
-                                        <input type="text" class="form-control input-sm" placeholder="Ingrese  nombre de clasificación." id="clasificacion" name="clasificacion">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                               <div class="">
-                            </div>
-                            </div>
-                            </div>
-                           <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <div class="form-example-int">
-                                    <div class="form-group">
-                                        <label>Correlativo:</label>
-                                        <div class="nk-int-st">
-                                        <input type="text" class="form-control input-sm" placeholder="Ingrese  un correlativo para departamento." id="correlativo" name="correlativo">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                               <div class="">
-                            </div>
-                            </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <div class="form-example-int">
-                                    <div class="form-group">
-                                        <label>Depreciación:</label>
-                                        <div class="nk-int-st">
-                                        <input type="text" class="form-control input-sm" placeholder="Ingrese el tiempo de años para la depreciación." id="depreciacion" name="depreciacion">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                               <div class="">
-                            </div>
-                            </div>
-                            </div>
-                           <div class="form-example-int mg-t-15">
-                            <button class="btn btn-success notika-btn-success" style="margin-left: 500px;" onclick="go();" >Guardar</button>
-                            <button type="button" class="btn btn-success notika-btn-success" onclick="back();">Cancelar</button>
+    }
+}
+?>
+                      </tbody>
+                            </table>
                         </div>
-                        </form>
                     </div>
                 </div>
             </div>
-          
-            
-            
+        </div>
     </div>
-    <!-- Form Examples area End-->
+    <!-- Data Table area End-->
+
     <!-- Start Footer area-->
     <?php include "footer.php";?>
     <!-- End Footer area-->
@@ -229,9 +255,9 @@ function back(){
 		============================================ -->
     <script src="js/sparkline/jquery.sparkline.min.js"></script>
     <script src="js/sparkline/sparkline-active.js"></script>
-    <!-- flot JS
+     <!-- flot JS
 		============================================ -->
-    <script src="js/flot/jquery.flot.js"></script>
+        <script src="js/flot/jquery.flot.js"></script>
     <script src="js/flot/jquery.flot.resize.js"></script>
     <script src="js/flot/flot-active.js"></script>
     <!-- knob JS
@@ -239,69 +265,62 @@ function back(){
     <script src="js/knob/jquery.knob.js"></script>
     <script src="js/knob/jquery.appear.js"></script>
     <script src="js/knob/knob-active.js"></script>
-    <!-- icheck JS
-		============================================ -->
-    <script src="js/icheck/icheck.min.js"></script>
-    <script src="js/icheck/icheck-active.js"></script>
-    <!--  wave JS
-		============================================ -->
-    <script src="js/wave/waves.min.js"></script>
-    <script src="js/wave/wave-active.js"></script>
     <!--  Chat JS
 		============================================ -->
     <script src="js/chat/jquery.chat.js"></script>
     <!--  todo JS
 		============================================ -->
     <script src="js/todo/jquery.todo.js"></script>
+	<!--  wave JS
+		============================================ -->
+    <script src="js/wave/waves.min.js"></script>
+    <script src="js/wave/wave-active.js"></script>
     <!-- plugins JS
 		============================================ -->
     <script src="js/plugins.js"></script>
-        <!-- Input Mask JS
+    <!-- Data Table JS
 		============================================ -->
-    <script src="js/jasny-bootstrap.min.js"></script>
-     <!-- bootstrap select JS
-		============================================ -->
-        <script src="js/bootstrap-select/bootstrap-select.js"></script>
-
+    <script src="js/data-table/jquery.dataTables.min.js"></script>
+    <script src="js/data-table/data-table-act.js"></script>
     <!-- main JS
 		============================================ -->
     <script src="js/main.js"></script>
 	<!-- tawk chat JS
 		============================================ -->
-    <!-- <script src="js/tawk-chat.js"></script> -->
+    <script src="js/tawk-chat.js"></script>
+     <!--  notification JS
+		============================================ -->
+        <script src="js/notification/bootstrap-growl.min.js"></script>
+    <script src="js/notification/notification-active.js"></script>
 </body>
 
 </html>
 <?php
 include "config/conexion.php";
-$bandera  = $_REQUEST["bandera"];
-$baccion = $_REQUEST["baccion"];
-$clasificacion     = $_REQUEST["clasificacion"];
-$depreciacion      = $_REQUEST["depreciacion"];
-$correlativo       = $_REQUEST["correlativo"];
-if($bandera=="add"){
-
-$query = "SELECT nombre FROM tclasificacion WHERE nombre like '%".$clasificacion."';";
-$result = $conexion->query($query);
-if($result->num_rows == 0){   
-$consulta  = "INSERT INTO tclasificacion VALUES('null','" .$clasificacion. "','" .$correlativo. "','" .$depreciacion. "')";
-    $resultado = $conexion->query($consulta);
-      if($resultado){
-          msg("Se agregaron los datos correctamente");
-      } else {
-          msg("Error al insertar los datos");
-      }
-}else{
-  msg("Esta clasificacion ya existe");
-}
-}
-
-  
-function msg($texto)
+$accion = $_REQUEST['bandera'];
+if($accion==1){
+      msgI("Se modificaron los datos con exito");
+  } else if($accion==2) {
+      msgE("cocurrio un error en el registro de los archivos");
+  }else if($accion==3) {
+    msgA("Los datos que se ingresaron ya existen");
+}   
+function msgI($texto)
 {
     echo "<script type='text/javascript'>";
-    echo "alert('$texto');";
-    echo "document.location.href='ingresarClasificacion.php';";
+    echo "notify('Exito','$texto','top', 'right', 'any', 'success');";
+    echo "</script>";
+}
+function msgA($texto)
+{
+    echo "<script type='text/javascript'>";
+    echo "notify('Advertencia','$texto','top', 'right', 'any', 'warning');";
+    echo "</script>";
+}
+function msgE($texto)
+{
+    echo "<script type='text/javascript'>";
+    echo "notify('Error','$texto','top', 'right', 'any', 'danger');";
     echo "</script>";
 }
 ?>
